63b8b6c810afdab47c0836d9a949c9e3e5ab4c84
dbc-apprentice <dbc-apprentice@devbootcamp.com>
2013-09-05 16:27:49 -0700

    deleted unnecessary files

--- app/models/comment.rb
+++
@@ -1,5 +0,0 @@
-class Comment < ActiveRecord::Base
-  # Remember to create a migration!
-  belongs_to :post
-  belongs_to :user
-end
--- app/models/post.rb
+++
@@ -1,5 +0,0 @@
-class Post < ActiveRecord::Base
-  # Remember to create a migration!
-  belongs_to :user
-  has_many :comments
-end
