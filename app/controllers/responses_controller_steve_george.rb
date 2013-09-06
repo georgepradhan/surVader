d48bdac91aba1a8b8110d66a77d1718a4afe780f
DBC Apprentice <apprentice@devbootcamp.com>
2013-09-06 16:37:21 -0700

    route to see survey results

--- app/controllers/responses_controller_steve_george.rb
+++ app/controllers/responses_controller_steve_george.rb
@@ -5,4 +5,12 @@ post '/survey/:survey_id/responses' do
   redirect "/survey/#{survey_id}/results"
 end
 
+get "/survey/:survey_id/results" do
+  @survey = Survey.find(params[:survey_id])
+  @questions = @survey.questions
+  @choices = @questions.map {|question| question.choices}  # array of choice-object arrays... 
+
+  erb :_results
+end
+
 
