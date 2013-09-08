ad77f3456d584d75ab3f91513fce20214f1c9816
Steven Swigart <sbswigart@gmail.com>
2013-09-08 16:50:15 -0700

    add static moris chart

---
+++ public/js/survey_results.js
@@ -0,0 +1,21 @@
+//survey_results.js
+console.log("LOADED")
+
+new Morris.Bar({
+  // ID of the element in which to draw the chart.
+  element: 'barGraph',
+  // Chart data records -- each entry in this array corresponds to a point on
+  // the chart.
+  data: [
+    { year: 'Darth Vader', value: 20 },
+    { year: 'JarJar Binks', value: 10 },
+    { year: 'Boba Fett', value: 5 }
+  ],
+  // The name of the data record attribute that contains x-values.
+  xkey: 'year',
+  // A list of names of data record attributes that contain y-values.
+  ykeys: ['value'],
+  // Labels for the ykeys -- will be displayed when you hover over the
+  // chart.
+  labels: ['Value']
+});
