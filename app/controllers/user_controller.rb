18f7714f8a42b9157bb54a8d0d07120188699a33
danapczynski <apczynski@gmail.com>
2013-09-08 13:48:55 -0700

    preparing merge to master

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -8,15 +8,10 @@ post "/users" do
   signup
 end
 
-get "/users/test" do
-  @user = User.find(current_user.id)
-  @surveys = @user.surveys
-  erb :user_dashboard
-end
-
 get "/users/:user_id" do
   @user = User.find(params[:user_id])
   @surveys = @user.surveys
-  erb :user_profile
+  # erb :user_profile << View used during Friday presentation
+  erb :user_dashboard
 end
 
