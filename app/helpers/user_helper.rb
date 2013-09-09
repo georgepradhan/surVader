d374c59714e4f5ab86cfe5d94afeb8e54f0d7a5d
George Pradhan <kgeorgepradhan@gmail.com>
2013-09-08 20:37:35 -0700

    implement gravatar image

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -18,5 +18,11 @@ helpers do
     end
   end
 
+  def gravatar_image_source
+    email = current_user.email
+    hash = Digest::MD5.hexdigest(email)
+    "http://www.gravatar.com/avatar/#{hash}"
+  end
+
 end
 
