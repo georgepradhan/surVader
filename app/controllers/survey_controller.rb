f965a11ad89e9ac8de68bb44f2ab4516b6313c15
dbc-apprentice <dbc-apprentice@devbootcamp.com>
2013-09-08 21:14:45 -0700

    delete old post routes in survey controller

--- app/controllers/survey_controller.rb
+++ app/controllers/survey_controller.rb
@@ -49,30 +49,5 @@ get "/surveys/:survey_id/results/questions/:question_id" do
   @choices.to_json
 end
 
-#Check that these routes are being used...
 
-post '/surveys' do
-  @survey = Survey.new(label: params[:label], description: params[:description], user: current_user)
-  if @survey.save
-    @confirmation = "You made the survey #{@survey.label}!"
-    # redirect to("/users/#{current_user.id}")
-    erb :_question_create, layout: false
-  else
-    @errors = @survey.errors.full_messages
-    erb :survey_create
-  end
-end
-
-post '/questions' do
-  @survey = Survey.find(params[:survey_id])
-  @question = Question.create(label: params[:label], survey: @survey)
-  erb :_choice_create, layout: false
-end
- 
-post '/choices' do
-  @survey = Survey.find(params[:survey_id])
-  @question = Question.find(params[:question_id])
-  @choice = Choice.create(label: params[:label], question: @question)
-  erb :_choice_create, layout: false
-end
 
